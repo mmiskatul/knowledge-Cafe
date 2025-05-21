f4143e3d435abbd878362323fc01089c400ae391
mmiskatul <masabimiskat@gmail.com>
2025-05-21 18:18:59 +0600

    final commit

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { GoBookmark } from 'react-icons/go';
 
-const Blog = ({blog}) => {
-    const {title,cover,author,authorImg,date,readTime,tags}=blog;
+const Blog = ({blog,handleAddtoBookMark,handleMarkRead}) => {
+    const {id,title,cover,author,authorImg,date,readTime,tags}=blog;
+
     return (
-        <div className='flex flex-col justify-center  px-20 mt-10  rounded-2xl hover:'>
+        <div className='flex flex-col justify-center  px-20 mt-10 mb-20  rounded-2xl '>
             {/* cover page */}
-            <div className='w-fit items-center'>
-                <img className='rounded-2xl mt-8'  src={cover} alt="" />
+            <div className='w-full items-center'>
+                <img  className='full rounded-2xl '  src={cover} alt={`Cover picture of the Title ${title}`} />
             </div>
             {/* description */}
-            <div className='px-2 py-3  '>
+            <div className='py-3  '>
                 {/* author */}
                 <div className='flex justify-between items-center mt-5 ' >
                 <div className='flex items-center gap-5 mt'>
-                    <div><img src={authorImg} className='size-12 rounded-full' alt="" /></div>
+                    <div><img src={authorImg} className='size-14 rounded-full' alt="" /></div>
                     <div>
                         <h5 className='text-2xl text-[#111111] font-bold'>{author}</h5>
                         <p className='text-sm font-semibold text-[#111111]/60'>{date}</p>
                     </div>
                     
                 </div>
-                <div className='flex items-center gap-2 text-[#111111]/60 text-xl'>{readTime} min read
-                    <button className='cursor-pointer text-3xl' ><GoBookmark /></button>
+                <div className='flex items-center gap-2 text-[#111111]/60 text-xl'>
+                        <span>{readTime} min read</span>
+                    <button onClick={()=>handleAddtoBookMark(blog)} className='cursor-pointer text-3xl' ><GoBookmark /></button>
                 </div>
                 
             </div>
@@ -31,8 +33,14 @@ const Blog = ({blog}) => {
                     <h1 className='text-4xl text-[#111111] font-bold my-7' >{title}</h1>
                
                 {/* Tags */}
-                <h4 className='font-medium text-xl text-[#111111]/60 mb-7'> {tags} </h4>
-                <button className='text-blue-500  cursor-pointer mb-10'> Mark as Read</button>
+                <p className='font-medium text-xl text-[#111111]/60 mb-7 space-x-2 flex gap-2'> 
+                    {
+                        tags.map((tag,index)=>(
+                           <span key={index}><a href="">{tag}</a></span>
+                        ))
+                    }
+                </p>
+                <button className='text-blue-500  cursor-pointer mb-10' onClick={()=>handleMarkRead(readTime,id)}> Mark as Read</button>
                 <hr  className='text-gray-400'/>
 
             </div>
@@ -41,7 +49,9 @@ const Blog = ({blog}) => {
     );
 };
 
-Blog.propTypes ={
-    blog: PropTypes.object.isRequired
-}
+    Blog.propTypes ={
+        blog: PropTypes.object.isRequired,
+        handleAddtoBookMark: PropTypes.func,
+        handleMarkRead: PropTypes.func
+    }
 export default Blog;
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Blog from '../Blog/Blog';
+import PropTypes from 'prop-types';
 
-function Blogs() {
+function Blogs({handleAddtoBookMark ,handleMarkRead}) {
     const [blogs,setBlogs]=useState([]);
     useEffect(()=>{
         fetch('blogs.json').then(res=>res.json()).then(data=>setBlogs(data));
@@ -10,10 +11,13 @@ function Blogs() {
     <div className='md:w-2/3'>
        {/* <h1 >blogs  :{blogs.length}</h1> */}
        {
-        blogs.map(blog=><Blog key={blog.id} blog={blog}/>)
+        blogs.map(blog=><Blog key={blog.id} handleAddtoBookMark={handleAddtoBookMark} handleMarkRead={handleMarkRead} blog={blog}/>)
        }
     </div>
   )
 }
-
+Blogs.prototype ={
+    handleAddtoBookMark: PropTypes.func,
+    handleMarkRead: PropTypes.func
+}
 export default Blogs
